c4c7f28be7ba59457e93672a2d2e19456b918728
Russell Newton <russell.newton01@gmail.com>
2021-12-31 16:41:04 -0500

    Upload to test json parsing

---
+++ assets/js/ghrepos.js
@@ -0,0 +1,33 @@
+(function ($) {
+
+    const baseHref = "https://https://github.com/Russell-Newton";
+    const baseSvgRef = "https://gh-card.dev/repos/Russell-Newton"
+
+    var $window = $(window),
+        $repos = $('#repos');
+
+    $.getJSON("assets/json/ghrepos.json", function (result) {
+        let repoData = result;
+
+        for (const repo of repoData.repos) {
+            let repoSection = document.createElement("section");
+
+            let repoTitle = document.createElement("h3");
+            repoTitle.innerHTML = repo.title;
+            repoSection.appendChild(repoTitle);
+
+            let repoDesc = document.createElement("p");
+            repoDesc.innerHTML = repo.blurb.join("\n");
+            repoSection.appendChild(repoDesc);
+
+            let repoCard = document.createElement("a");
+            repoCard.href = `${baseHref}/${repo.repo}`;
+            repoCard.target = "_blank";
+            repoCard.rel = "noopener noreferrer";
+            repoCard.innerHTML = `<img src="${baseSvgRef}/${repo.title}.svg">`;
+            repoSection.appendChild(repoCard);
+
+            $repos.appendChild(repoSection);
+        }
+    });
+})(jQuery);
