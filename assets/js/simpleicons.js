739e6f3e3a4abc1d77d31271a07c175a5e6e8eb3
Russell Newton <russell.newton01@gmail.com>
2022-01-01 19:20:53 -0500

    Update icons, add skills

--- assets/js/simpleicons.js
+++ assets/js/simpleicons.js
@@ -3,8 +3,19 @@
 
     $("si-icon").each(function () {
         let slug = $(this).attr("slug");
-        console.log(slug);
-        let replacement = `<img src="${baseSrc}/${slug}.svg" onload="SVGInject(this)" />`;
+        let attrs = $(this).prop("attributes");
+        let attrsString = "";
+        $.each(attrs, function() {
+            attrsString += `${this.name}="${this.value}" `;
+        })
+        // [...$(this).attributes].forEach(attr => {
+        //     attrsString += `${attr.nodeName}="${attr.nodeValue}" `
+        // })
+        // for (let attr in attrs) {
+        //     attrsString += `${this.name}="${this.value}" `;
+        // }
+        let replacement = `<img src="${baseSrc}/${slug}.svg" onload="SVGInject(this, {copyAttributes: true})" ${attrsString}/>`;
+        console.log(replacement);
         $(this).replaceWith(replacement);
     });
 })(jQuery);
